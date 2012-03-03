811ea0b5f4f01bbf61b0a2c3b5e77e14b681cd0f
Tomasz Guziaek <tomasz@guzialek.info>
2012-03-03 20:58:46 +0100

    determineTreeWidth changed into determineTreeLevels: it return nodes grouped by tree level.

--- src/svgcreator/__init__.py
+++ src/svgcreator/__init__.py
@@ -10,6 +10,7 @@ class SVGTreeCreator:
     def __init__(self, conf):
         self.__conf = conf
         self.__nodeDefaultWidth = 100
+        self.__nodeDefaultHeight = 100
         
         self.prepareSVGObject()
         self.prepareShapeBuilder()
@@ -26,53 +27,55 @@ class SVGTreeCreator:
     def createSVGFile(self, fileName):
         self.__SVGObject.save(fileName)
         
-    def determineTreeWidth(self, data, level = 1, result = None):
+    def determineTreeLevels(self, data, level = 0, result = None):
         '''
-        Counts nodes (type = 'node', excluding type = 'reference') on each depth level.
+        Grouping nodes according to depth levels. Considering type = 'node', excluding type = 'reference'.
     
         @param data: root node of tree or subtree
         @param level: depth level of node investigated currently
-        @param result: dictionary with with levels as keys and number of nodes on this level as values
+        @param result: dictionary with with levels as keys and list of nodes on this level as values
     
         @return: updated result
         '''
         if data['type'] == 'node':
-            if result == None and level == 1:
-                result = dict()
-                result[1] = 1
+            if result == None and level == 0:
+                result = dict()                
                 
-            if level != 1 and result != None:
-                if result.__contains__(level):
-                    result[level] += 1
-                else:
-                    result[level] = 1
+            if not result.__contains__(level):
+                result[level] = dict()
+            result[level][len(result[level].keys())] = data
                     
             if data.has_key('children'):
                 for child in data['children']:
-                    result = self.determineTreeWidth(child, level + 1, result)
+                    result = self.determineTreeLevels(child, level + 1, result)
                                 
         return result
         
     def prepareTree(self, rootNode):
-        treeWidth = self.determineTreeWidth(rootNode)
+        treeLevels = self.determineTreeLevels(rootNode)
         
-        maxWidth = 0
-        for level in treeWidth.keys():
-            if treeWidth[level] > maxWidth:
-                maxWidth = treeWidth[level]
+        self.prepareTreeLevels(treeLevels)
         
-        self.prepareNode(rootNode, maxWidth / 2 * (self.__nodeDefaultWidth + 150), 50)
-        
-        if rootNode['type'] == 'node':
-            if rootNode.has_key('children'):
-                i = 0
-                for child in rootNode['children']:
-                    self.prepareNode(child, i * (self.__nodeDefaultWidth + 150), 2 * 150)
-                    i += 1
+#        if rootNode['type'] == 'node':
+#            if rootNode.has_key('children'):
+#                i = 0
+#                for child in rootNode['children']:
+#                    self.prepareNode(child, i * (self.__nodeDefaultWidth + 150), 2 * 150)
+#                    i += 1
+    
+    def prepareTreeLevels(self, treeLevels):
+        for level in treeLevels.keys():
+            self.prepareTreeLevel(treeLevels[level], level)
+    
+    def prepareTreeLevel(self, nodes, level):
+        i = 0
+        for node in nodes.values():
+            self.prepareNode(node, i * (self.__nodeDefaultWidth + 150), level * 120)
+            i += 1
         
     def prepareNode(self, node, startX, startY):
         width = self.__nodeDefaultWidth
-        height = 100
+        height = self.__nodeDefaultHeight
         
         nodeGroup = g()
         
